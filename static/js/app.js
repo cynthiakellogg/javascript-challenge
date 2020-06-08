d3bf3238e634429310f93d2fbf1f647e457f4ec1
Cynthia <59157682+cynthiakellogg@users.noreply.github.com>
2020-06-08 11:59:17 -0700

    added data to table in html

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,15 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+var tbody = d3.select("tbody");
+tableData.forEach(function(siting) {
+    console.log(siting);
+    var row = tbody.append("tr");
+    Object.entries(siting).forEach(function([key, value]) {
+            console.log(key, value);
+            var cell = row.append("td");
+            cell.text(value);
+          });
+});
+
+// * Use a date form in your HTML document and write JavaScript code that will listen for events and search through the `date/time` column to find rows that match user input.
