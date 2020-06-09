3a8d2a5907915fef24a95709dca041cf1b24fe39
Cynthia <59157682+cynthiakellogg@users.noreply.github.com>
2020-06-08 19:44:44 -0700

    playing around with try and catch handling in javascript

--- static/js/app.js
+++ static/js/app.js
@@ -2,17 +2,16 @@
 var tableData = data;
 var tbody = d3.select("tbody");
 
-var input = d3.select("#datetime");
-input.on("change", newInput);
+var button = d3.select("#filter-btn");
+button.on("click", newInput);
 
 function newInput() {
     tbody.html("");
     d3.event.preventDefault();
     var inputField = d3.select("#datetime");
     var inputValue = inputField.property("value");
-    // var newText = d3.event.target.value;
     var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
-    console.log(filteredData);
+    // console.log(filteredData);
     // now we want to 
     filteredData.forEach(function(siting) {
         var row = tbody.append("tr");
@@ -23,3 +22,13 @@ function newInput() {
     });
   };
 
+//   try { 
+//     if(x == "")  throw "empty";
+//     if(isNaN(x)) throw "not a number";
+//     x = Number(x);
+//     if(x < 5)  throw "too low";
+//     if(x > 10)   throw "too high";
+//   }
+//   catch(err) {
+//     message.innerHTML = "Input is " + err;
+//   }
