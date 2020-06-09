3b27f810f48c30767aa4f974168fa00fccd54b80
Cynthia <59157682+cynthiakellogg@users.noreply.github.com>
2020-06-08 18:21:05 -0700

    working

--- static/js/app.js
+++ static/js/app.js
@@ -2,14 +2,32 @@
 var tableData = data;
 
 var tbody = d3.select("tbody");
-tableData.forEach(function(siting) {
-    console.log(siting);
-    var row = tbody.append("tr");
-    Object.entries(siting).forEach(function([key, value]) {
-            console.log(key, value);
-            var cell = row.append("td");
-            cell.text(value);
-          });
-});
 
-// * Use a date form in your HTML document and write JavaScript code that will listen for events and search through the `date/time` column to find rows that match user input.
+
+
+
+// filteredTableData = [];
+
+var input = d3.select("#datetime");
+input.on("change", newInput);
+
+function newInput() {
+    d3.event.preventDefault();
+    var inputField = d3.select("#datetime");
+    var inputValue = inputField.property("value");
+    // var newText = d3.event.target.value;
+    var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
+    console.log(filteredData);
+    // now we want to 
+    filteredData.forEach(function(siting) {
+        var row = tbody.append("tr");
+        Object.entries(siting).forEach(function([key, value]) {
+                var cell = row.append("td");
+                cell.text(value);
+              });
+    });
+  };
+
+//
+  
+//   var filteredData = tableData.filter(tableData => tableData.date === inputValue);
